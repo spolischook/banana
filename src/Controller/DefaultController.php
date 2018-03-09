66d86d66f3a7afb0fbf3309495c78d329d170a83
Serhii Polishchuk <spolischook@gmail.com>
2018-03-09 22:55:25 +0700

    Hello world

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("")
+     */
+    public function homeAction()
+    {
+        return new Response('Home action is work');
+    }
+}
